205542553e4daaed87c226602bf2c7c2e903e129
Francisco Marcano <fjmn2001@gmail.com>
2020-03-21 21:10:02 -0400

    adding vou route v4

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -4,6 +4,7 @@ import VueRouter from 'vue-router';
 import Login from "../components/auth/Login";
 import Home from "../components/Home";
 import Logout from "../components/auth/Logout";
+import store from "../store";
 
 Vue.use(VueRouter);
 
@@ -14,18 +15,45 @@ const router = new VueRouter({
             path: '/login',
             name: 'login',
             component: Login,
+            meta: {
+                requiresVisitor: true
+            }
         },
         {
             path: '/logout',
             name: 'logout',
             component: Logout,
+            meta: {
+                requiresAuth: true
+            }
         },
         {
             path: '/',
             name: 'home',
             component: Home,
+            meta: {
+                requiresAuth: true
+            }
         },
     ]
+});
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        if (!store.getters.isLogged) {
+            next({name: 'login'})
+        } else {
+            next();
+        }
+    } else if (to.matched.some(record => record.meta.requiresVisitor)) {
+        if (store.getters.isLogged) {
+            next({name: 'home'})
+        } else {
+            next();
+        }
+    } else {
+        next();
+    }
 });
 
 export default router;
